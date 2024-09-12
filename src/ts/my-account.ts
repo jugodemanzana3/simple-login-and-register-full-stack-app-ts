3def0a13bcd213e79ed33cd8f95487f4a3054489
Christian <destructordemundos3@outlook.com>
2024-09-11 21:03:30 -0500

    4

--- src/ts/my-account.ts
+++ src/ts/my-account.ts
@@ -32,14 +32,27 @@ const handleLogoutButton = () => {
   const url = `${config.SERVER_URL}/api/auth/logout`;
 
   const logout = async () => {
+    const alertMessage = document.querySelector('.alert-message') as HTMLElement;
+
     try {
       const res = await fetch(url, {
         method: 'POST',
         credentials: 'include',
       });
 
+      const data = await res.json();
+
       if (res.ok) {
-        window.location.href = './login.html';
+        alertMessage.textContent = data.message;
+        alertMessage.classList.add('visible');
+
+        setTimeout(() => {
+          alertMessage.classList.remove('visible');
+        }, 3000);
+
+        setTimeout(() => {
+          window.location.href = './login.html';
+        }, 3000);
       } else {
         console.log('Error');
       }
